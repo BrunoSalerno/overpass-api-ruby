e77a369a08ad8a09ac8fe8cc319cf04d75282d10
Bruno Salerno <br.salerno@gmail.com>
2017-05-19 23:13:03 -0300

    Adds QL support

--- lib/overpass_api_ruby.rb
+++ lib/overpass_api_ruby.rb
@@ -1,1 +1,2 @@
 require_relative "xml"
+require_relative "ql"
--- spec/overpass_api_ruby_spec.rb
+++ spec/overpass_api_ruby_spec.rb
@@ -4,14 +4,29 @@ require 'overpass_api_ruby'
 describe OverpassAPI do
   describe OverpassAPI::XML do
     it "'s example works" do
-      ba_query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
-        "</union><union><item/><recurse type='down'/></union>"
-
       options={:bbox => {:s => -34.705448, :n => -34.526562,
                          :w => -58.531471, :e => -58.335159},
                :timeout => 900}
 
       overpass = OverpassAPI::XML.new(options)
+
+      ba_query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
+        "</union><union><item/><recurse type='down'/></union>"
+
+
+      expect(overpass.query(ba_query)[:elements]).to be_a(Array)
+    end
+  end
+
+  describe OverpassAPI::QL do
+    it "'s example works" do
+      options={:bbox => {:s => -34.705448, :n => -34.526562,
+                         :w => -58.531471, :e => -58.335159},
+               :timeout => 900}
+
+      overpass = OverpassAPI::QL.new(options)
+
+      ba_query = "rel['route'='subway'];(._;>;);out body;"
 
       expect(overpass.query(ba_query)[:elements]).to be_a(Array)
     end
