91185245fbd168f3b158a0f00acc92b0ae7f04be
Bruno Salerno <br.salerno@gmail.com>
2017-05-19 22:53:40 -0300

    Adds specific XML class

---
+++ lib/base.rb
@@ -0,0 +1,40 @@
+require 'httpi'
+require 'open-uri'
+require 'json'
+
+module OverpassAPI
+  VERSION='0.2'
+
+  class Base
+    DEFAULT_ENDPOINT='http://overpass-api.de/api/interpreter?data='
+
+    def initialize(args={})
+      bbox = args[:bbox]
+      bounding_box(bbox[:s],bbox[:n],bbox[:w],bbox[:e]) if bbox
+
+      @endpoint = args[:endpoint] || DEFAULT_ENDPOINT
+      @timeout = args[:timeout]
+      @maxsize = args[:maxsize]
+    end
+
+    def bounding_box(s,n,w,e)
+      @bbox = "#{s},#{w},#{n},#{e}"
+    end
+
+    def query(q)
+      perform build_query(q)
+    end
+
+    def raw_query(q)
+      perform q
+    end
+
+    private
+
+    def perform(q)
+      url = URI::encode("#{@endpoint}#{q}")
+      r = HTTPI::Request.new(url)
+      JSON.parse(HTTPI.get(r).body, symbolize_names: true)
+    end
+  end
+end
--- spec/overpass_api_ruby_spec.rb
+++ spec/overpass_api_ruby_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 require 'overpass_api_ruby'
 
 describe OverpassAPI do
-  it "'s example works" do
-    ba_query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
-      "</union><union><item/><recurse type='down'/></union>"
+  describe OverpassAPI::XML do
+    it "'s example works" do
+      ba_query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
+        "</union><union><item/><recurse type='down'/></union>"
 
-    options={:bbox => {:s => -34.705448, :n => -34.526562,
-                       :w => -58.531471, :e => -58.335159},
-             :timeout => 900}
+      options={:bbox => {:s => -34.705448, :n => -34.526562,
+                         :w => -58.531471, :e => -58.335159},
+               :timeout => 900}
 
-    overpass = OverpassAPI.new(options)
+      overpass = OverpassAPI::XML.new(options)
 
-    expect(overpass.query(ba_query)[:elements]).to be_a(Array)
+      expect(overpass.query(ba_query)[:elements]).to be_a(Array)
+    end
   end
 end
